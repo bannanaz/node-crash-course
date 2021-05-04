c4d8b5d71ef351de68ec3d3789c47a4d82858183
bannanaz <anlepet@gmail.com>
2021-05-04 15:05:47 +0200

    global object, methods, files, streams, pipes"

---
+++ global.js
@@ -0,0 +1,25 @@
+/*const name = 'Eggbert'
+console.log(name)
+
+const greet = (name) => {
+    console.log(`hello, ${name}`)
+}
+
+greet ('Anna');
+greet ('Olle');*/
+
+//Global object
+//console.log(global);
+
+// global.setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval (() => {
+//     console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
---
+++ people.js
@@ -0,0 +1,7 @@
+// const people = ['Anna', 'Olle', 'Otto']
+// const ages = [40, 48, 9]
+
+
+// module.exports = {
+//     people, ages
+// };
